2a8cb8ebc5ffe97fc396ba732e2a902bcc6d7f9a
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2024-03-26 12:48:17 +0000

    add rust lib

---
+++ rust-wasm/src/lib.rs
@@ -0,0 +1,19 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    log("Hello, quik!");
+}
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
